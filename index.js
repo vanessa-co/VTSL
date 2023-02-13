fb376b96dc77447ace7b59b34d831b2dba01539f
Vanessa <vanessa.co403@gmail.com>
2023-02-13 11:36:59 -0500

    fixed API URL

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 .then((response) => response.json())
 .then((data) => {
     console.log(data);
