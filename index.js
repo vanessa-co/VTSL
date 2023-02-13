6b591560ff64e2561b9a8354753c849ecfa4215a
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-02-13 12:59:54 -0500

    2nd commit from Tim git push ls dafskj

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-.then((response) => response.json())
-.then((data) => {
-    console.log(data);
- 
-}) 
+const searchInput = document.getElementById("searchInput"); 
+const searchButton = document.getElementById("drink-button");
+
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+.then(response => response.json())
+.then(response => console.log(response))
+// .then(drinksArray => {
+//     drinksArray.forEach(drinkObject => {
+//         renderDrink(drinkObject)
+//         console.log(renderDrink[0]); 
+//     });
+// });
+
+
+
+// searchButton.addEventListener("click", () => {
+//     const searchButton = searchInput.value;
+//     getCocktails(searchTerm);
+//     console.log("does this work?")
+//   });
 
