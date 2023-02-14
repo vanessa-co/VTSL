c5e3e438908b2ee1eb9bee4a78f595f2f14a5bed
Scott <scotthenry1@gmail.com>
2023-02-14 11:50:57 -0500

    Added mouseover functionality for drink cards

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ function clearRecipeCardsContainer() {
   recipeCardsContainer.innerHTML = '';
 }
 
+//function to handle a user mousing over a recipe card
+function handleRecipeCardMouseOver(event) {
+  //window.alert('Recipe card hovered!');
+  //recipeCardElement.innerHTML = "test";
+  let target = event.target;
+  let parent = target.parentElement;
+  console.log(parent);
+  console.log(target);
+  console.log(drink);
+  //target.innerHTML = "test";
+  //const elementToUpdate = event;
+  //elementToUpdate.innerHTML = "test";
+}
+
 // function to add a recipe card for a drink to the recipe cards container
 function addRecipeCard(drink) {
   // Create a new recipe card element
@@ -32,6 +46,27 @@ function addRecipeCard(drink) {
 
   // Set the inner HTML of the recipe card to display the drink information
   recipeCardElement.innerHTML = buildRecipeCardHTML(drink);
+  
+  recipeCardElement.addEventListener("mouseover", function (event) {
+    let target = event.target;
+    let parent = target.parentElement;
+    let ingredientArray = [];
+    
+    recipeCardElement.innerHTML =  `<h2 align="center">Ingredients:</h2>`
+    const loopTimes = 16;
+    for (let i = 1; i < loopTimes; i++) {
+      let ingredient = "strIngredient" + i;
+      if (drink[ingredient] != null) {
+        //ingredientArray.push(ingredient.strIngredient[i]);
+        recipeCardElement.innerHTML += `<p align="center">${drink[ingredient]}</p>`
+    }
+  }});
+  
+  recipeCardElement.addEventListener("mouseout", function (event) {
+    let target = event.target;
+    let parent = target.parentElement;
+    recipeCardElement.innerHTML = buildRecipeCardHTML(drink);
+  })
 
   // Append the recipe card to the recipe cards container
   recipeCardsContainer.appendChild(recipeCardElement);
@@ -41,10 +76,7 @@ function addRecipeCard(drink) {
 function buildRecipeCardHTML(drink) {
   return `
     <img src="${drink.strDrinkThumb}" />
-    <h2>${drink.strDrink}</h2>
-    <div class="recipe-details">
-      <p>${drink.strInstructions}</p>
-    </div>
+    <h2 align="center">${drink.strDrink}</h2>
   `;
 }
 
