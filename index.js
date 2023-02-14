1097fe1ffe97a399829e9e5186c749231ae82345
Vanessa <vanessa.co403@gmail.com>
2023-02-14 00:25:16 -0500

    money moves

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-const searchInput = document.getElementById("searchInput"); 
-const searchButton = document.getElementById("drink-button");
+const searchButtonElement = document.querySelector('#search-button');
+const searchInputElement = document.querySelector('#search-bar');
+const recipeCardsContainer = document.querySelector('.recipes-container');
 
+// function to handle the search button click
+function handleSearchButtonClick() {
+  // Get the value of the search bar
+  const searchTerm = searchInputElement.value;
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-.then(response => response.json())
-.then(response => console.log(response))
-// .then(drinksArray => {
-//     drinksArray.forEach(drinkObject => {
-//         renderDrink(drinkObject)
-//         console.log(renderDrink[0]); 
-//     });
-// });
+  // Use the fetch API to get data from the Cocktail API
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    .then((response) => response.json()) // Parse the response as JSON
+    .then((data) => {
+      // Clear the recipe cards container
+      clearRecipeCardsContainer();
 
+      // Loop through the drinks in the data and add a recipe card for each one
+      data.drinks.forEach(addRecipeCard);
+    });
+}
 
+// function to clear the recipe cards container
+function clearRecipeCardsContainer() {
+  recipeCardsContainer.innerHTML = '';
+}
 
-// searchButton.addEventListener("click", () => {
-//     const searchButton = searchInput.value;
-//     getCocktails(searchTerm);
-//     console.log("does this work?")
-//   });
+// function to add a recipe card for a drink to the recipe cards container
+function addRecipeCard(drink) {
+  // Create a new recipe card element
+  const recipeCardElement = document.createElement('div');
+  recipeCardElement.classList.add('recipe-card');
 
+  // Set the inner HTML of the recipe card to display the drink information
+  recipeCardElement.innerHTML = buildRecipeCardHTML(drink);
+
+  // Append the recipe card to the recipe cards container
+  recipeCardsContainer.appendChild(recipeCardElement);
+}
+
+// function to build the HTML for a recipe card based on a drink object
+function buildRecipeCardHTML(drink) {
+  return `
+    <img src="${drink.strDrinkThumb}" />
+    <h2>${drink.strDrink}</h2>
+    <div class="recipe-details">
+      <p>${drink.strInstructions}</p>
+    </div>
+  `;
+}
+
+// Add a click event listener to the search button
+searchButtonElement.addEventListener('click', handleSearchButtonClick);
